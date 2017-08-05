abce531b0240d4b77a42a9def86e55f253dcede1
GenaBitu <genabitu@gmail.com>
2017-08-05 21:28:22 +0200

    Fixed file not found error

--- lib_resources/print_los.lua
+++ lib_resources/print_los.lua
@@ -1,16 +1,18 @@
 function print_los(jobname)
-	local output = ""
-	lines = {}
-	for line in io.lines(jobname .. ".los") do
-		number, name = line:match("([^|]+)|([^|]+)")
-		lines[name] = tonumber(number)
+	if io.open(jobname .. ".los", "r") ~= nil then
+		local output = ""
+		lines = {}
+		for line in io.lines(jobname .. ".los") do
+			number, name = line:match("([^|]+)|([^|]+)")
+			lines[name] = tonumber(number)
+		end
+		sorted = {}
+		for n in pairs(lines) do table.insert(sorted, n) end
+		table.sort(sorted)
+		for _, name in ipairs(sorted) do
+			output = output .. "\\songlistentry{" .. lines[name] .. "}{" .. name .. "}\n"
+		end
+		los = io.open(jobname .. ".los", "w")
+		tex.print(output)
 	end
-	sorted = {}
-	for n in pairs(lines) do table.insert(sorted, n) end
-	table.sort(sorted)
-	for _, name in ipairs(sorted) do
-		output = output .. "\\songlistentry{" .. lines[name] .. "}{" .. name .. "}\n"
-	end
-	los = io.open(jobname .. ".los", "w")
-	tex.print(output)
 end
